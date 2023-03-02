e1b60f832c1e7a976cdbb70de6e04988787ec58b
chiquinquira quintero <sciencesmary@gmail.com>
2023-03-02 11:01:22 -0300

    segunda entrega responsive

--- main.js
+++
@@ -1,9 +0,0 @@
-const contenedor = document.querySelector('#contenedor');
-
-window.addEventListener('resize', () => {
-    if(window.innerWidth <= 768){
-        contenedor.classList.remove('active');
-    } else {
-        contenedor.classList.add('active');
-    }
-});
